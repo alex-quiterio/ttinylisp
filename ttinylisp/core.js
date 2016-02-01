e547d16d8b0f755a552507d1d5ecfee23f26964d
Alex Quiterio <afp.quiterio@gmail.com>
2016-02-01 14:10:46 +0000

    update core

--- ttinylisp/core.js
+++ ttinylisp/core.js
@@ -14,32 +14,44 @@ class Core {
         '<=': Core.le,
         '=':  Core.eq,
         '**':  Core.pow,
+        'len' : Core.list_length,
         'car' : Core.car,
         'cdr' : Core.cdr,
-        'let' : Core.define_variable,
         'print' : Core.print,
+        'let' : Core.define_variable,
+        'if'  : Core.if_function,
         'define' : Core.define_variable
     }
   }
 
+  static specialTokens() {
+    return ['if', 'let', 'define'];
+  }
+
   static print (x) { console.log(x); }
-  static car(list) { return list[0]; }
-  static cdr(list) { return list.slice(1); }
+
+  static car (list) { return list[0]; }
+  static cdr (list) { return list.slice(1); }
+
 	static sub (x, y) { return (x - y); }
   static add (x, y) { return (x + y); }
   static mul (x, y) { return (x * y); }
   static div (x, y) { return (x / y); }
   static pow (x, y) { return Math.pow(x, y); }
-  static gt (x, y) { return (x > y); }
-  static lt (x, y) { return (x < y); }
-  static ge (x, y) { return (x >= y); }
-  static le (x, y) { return (x <= y); }
-  static eq (x, y) { return (x === y); }
+  static gt  (x, y) { return (x > y); }
+  static lt  (x, y) { return (x < y); }
+  static ge  (x, y) { return (x >= y); }
+  static le  (x, y) { return (x <= y); }
+  static eq  (x, y) { return (x === y); }
 
 
-  static define_variable(lasign, rasign) {
-    this.set(lasign, rasign);
-    return rasign;
+  static define_variable(lasign, rasign, ev) {
+    this.set(lasign.lexeme, ev.eval(rasign));
+    return this.find(lasign.lexeme);
+  }
+  static list_length(list, ev) { return list.length; }
+  static if_function(test, positive, negative, ev) {
+    return ev.eval(test, this) ? ev.eval(positive, this) : ev.eval(negative, this);
   }
 }
 
--- ttinylisp/evaluator.js
+++ ttinylisp/evaluator.js
@@ -29,23 +29,29 @@ class Evaluator {
   }
 
   eval(expression, scope) {
-    console.log(scope)
     if (scope === undefined) { scope = this.defaultScope; }
     if (Utils.is_a('Array', expression)) {
       return this.evalList(expression, scope);
     } else if (expression.name === 'identifier' || expression.name === 'operator') {
-      return scope.find(expression.lexeme) || expression.lexeme;
+      return scope.find(expression.lexeme);
     } else {
       return expression.lexeme;
     }
   }
 
-  evalList(listExpressions, scope) {
-    let list = listExpressions.map((exp, _) => this.eval(exp, scope));
-    if (Utils.is_a('Function', list[0])) {
-      return list[0].apply(scope, list.slice(1))
+  evalList(expressionsList, scope) {
+    if (Core.specialTokens().indexOf(expressionsList[0].lexeme) > -1) {
+      let _func      = scope.find(expressionsList[0].lexeme);
+      let _arguments = expressionsList.slice(1).concat(this);
+      return _func.apply(scope, _arguments);
     } else {
-      return list;
+      let _evaluated = expressionsList.map((exp, _) => this.eval(exp, scope));
+      if (Utils.is_a('Function', _evaluated[0])) {
+        let _func      = _evaluated[0];
+        let _arguments = _evaluated.slice(1).concat(this);
+        return _func.apply(scope, _arguments);
+      }
+      return _evaluated;
     }
   }
 }
